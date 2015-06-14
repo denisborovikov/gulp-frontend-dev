78d92c50856c39ee9ead945233e5c780a9ea91dc
Denis Borovikov <metaller.ru@gmail.com>
2015-06-14 23:14:22 +0300

    Add jade support

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -3,12 +3,13 @@ var reload = browserSync.reload;
 
 module.exports = function (gulp, options, $) {
     gulp.task('html', function () {
-        return gulp.src(options.paths.html + '*.html')
+        return gulp.src(options.paths.html + '*.+(html|jade)')
             .pipe($.plumber())
             .pipe($.fileInclude({
                 prefix  : '@@',
                 basepath: '@file'
             }))
+            .pipe($.if('*.jade', $.jade({pretty: '  '})))
             .pipe($.htmlI18n({
                 langDir       : './lang',
                 createLangDirs: true
