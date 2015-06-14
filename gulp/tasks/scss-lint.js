ded0706b81aa845ad0353442aa1fcac07ffb9a20
Denis Borovikov <metaller.ru@gmail.com>
2015-06-14 17:18:42 +0300

    Update linter config

--- gulp/tasks/scss-lint.js
+++ gulp/tasks/scss-lint.js
@@ -3,20 +3,20 @@
 var chalk = require('chalk');
 var path = require('path');
 
-module.exports = function (gulp, options, $) {
-    gulp.task('lint', function () {
+module.exports = function(gulp, options, $) {
+    gulp.task('lint', function() {
         return gulp.src(options.linter)
             .pipe($.scssLint({
-                'config'      : './gulp/utils/scss-lint.yml',
-                'customReport': customReporter
-            }));
+                      'config'      : './gulp/utils/scss-lint.yml',
+                      'customReport': customReporter
+                  }));
     });
 
     function customReporter(file) {
         if (!file.scsslint.success) {
-            console.log(file.scsslint.issues.length + ' issues in '+ chalk.yellow((file.relative)));
+            console.log(file.scsslint.issues.length + ' issues in ' + chalk.yellow((file.relative)));
 
-            file.scsslint.issues.forEach(function (issue) {
+            file.scsslint.issues.forEach(function(issue) {
                 var linter = issue.linter ? (issue.linter + ': ') : '';
                 var logMsg = chalk.magenta(issue.line) + ': ' + chalk.green(linter) + issue.reason;
                 console.log(logMsg);
