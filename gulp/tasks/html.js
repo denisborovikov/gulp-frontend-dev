9859798db7fa0ea5a671bf37ba820e5b1d954254
Denis Borovikov <metaller.ru@gmail.com>
2015-06-14 23:30:29 +0300

    Place jade compiler call before gulp file include
    It makes possible to use html and jade templates at the same time. Use `gulp-file-include` to include raw html. Use jade `include` directive to include jade files.

--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -5,11 +5,11 @@ module.exports = function (gulp, options, $) {
     gulp.task('html', function () {
         return gulp.src(options.paths.html + '*.+(html|jade)')
             .pipe($.plumber())
+            .pipe($.if('*.jade', $.jade({pretty: '  '})))
             .pipe($.fileInclude({
                 prefix  : '@@',
                 basepath: '@file'
             }))
-            .pipe($.if('*.jade', $.jade({pretty: '  '})))
             .pipe($.htmlI18n({
                 langDir       : './lang',
                 createLangDirs: true
